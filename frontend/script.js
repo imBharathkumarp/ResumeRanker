d8af35ea48b58433a033368611cc847573f5d720
rahul21-7 <dasari2007rahul2007@gmail.com>
2025-06-06 13:25:02 +0530

    Dark mode added and the background of the card will change color now

--- frontend/script.js
+++ frontend/script.js
@@ -4,6 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const spinner = submitBtn.querySelector('.spinner-border');
     const results = document.getElementById('results');
     const error = document.getElementById('error');
+    const themeButton = document.getElementById('themeButton');
+    const body = document.querySelector('body');
+    const card = document.querySelector('#contCard');
+    const uploadCont = document.querySelector(".upload-container");
+    
+
+    var darkMode = false;
+    themeButton.addEventListener('click', (event)=>{
+        darkMode = !darkMode;
+        if(darkMode){
+            themeButton.classList.remove("darkButton");
+            themeButton.classList.add("lightButton");
+            themeButton.innerText="Light";
+            body.classList.remove("light-body");
+            body.classList.add("dark-body");
+            card.classList.remove('card');
+            card.classList.add('dark-card');
+            uploadCont.classList.remove('card');
+            uploadCont.classList.add('dark-card');
+        }
+        else{
+            themeButton.classList.remove("lightButton");
+            themeButton.classList.add("darkButton");
+            themeButton.innerText="Dark";
+            body.classList.remove("dark-body");
+            body.classList.add("light-body");
+            card.classList.remove('dark-card');
+            card.classList.add('card');
+        }
+    })
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
